7f07ce97b9c12244053db90418c64f74859be1b9
Josef Holzmayr <jester@theyoctojester.info>
2022-06-09 13:47:53 +0000

    entrypoint.sh: add shebang

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,5 @@
+#!/bin/sh -l
+
 JWT=$(curl -s -X POST -u ${INPUT_MENDER_USER}:${INPUT_MENDER_PASSWORD} ${INPUT_MENDER_URI}/api/management/v1/useradm/auth/login)
 
 curl -s -X POST ${INPUT_MENDER_URI}/api/management/v1/deployments/artifacts \
